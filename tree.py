220eae8854044658b52b86947e58e3cc5519a842
AdaAtess <aates@oberlin.edu>
2022-11-05 17:28:41 -0400

    entropy implemented

--- tree.py
+++ tree.py
@@ -6,6 +6,38 @@ d. Either True or False indicating whether we should handle numeric attributes a
 numeric (if False, then we treat them as categorical values)
 """
 import sys
+import numpy as np
+import pandas as pd
+
+# calculate getEntropy
+def getEntropy(training_set, uniqueLabels):
+    rows = training_set.shape[0]
+    class_entropy = 0
+    total_entropy = 0
+    for label in uniqueLabels:
+        #print(label)
+        count_l = 0
+        for insta_count in range(rows):
+            if training_set[insta_count][0] == label:
+                count_l += 1
+        #print(f"count of category:{count_l} and category is{label}")
+        class_entropy = - (count_l/rows) * np.log2(count_l/rows)
+        #print("class entropy is: ", class_entropy)
+        total_entropy += class_entropy
+        #print("total entropy is: ", total_entropy)
+
+    return total_entropy
+
+# calculate information gain
+
+# get feature max info
+
+#id3
+## label_list = training_set[0][:]
+## labels_unique = training_set[0][:].unique #so that we can calculate entropy for each label
+## label_count = length(label_list)
+
+# evaluate
 
 # Beginning of code
 try:
@@ -41,6 +73,35 @@ try:
     # Print all input values given for user to see
     print(f"Inputs:\nFile: {file_path}\nTraining set percent: {training_set_percent}")
     print(f"Random Seed: {randomSeed}\nHandle numeric as numeric: {handle_numeric}")
+
+    # Read in dataset
+    df = pd.read_csv(file_path)
+    # labels = df.iloc[:, 0]
+
+    # shuffle the dataframe. Use random seed from input and fraction 1 as we want the whole dataframe
+    shuffled_df = df.sample(frac=1, random_state=randomSeed)
+
+    print(f"Number of Instances in Dataframe: {len(df)}")
+
+    splits_indices = [int(training_set_percent * len(df))]
+    print(f"Splits indexes they begin at: {splits_indices}\n")
+    training_set, testing_set = np.split(shuffled_df, splits_indices)
+
+    # Print out the lengths of the training, validation, and testing sets
+    print(f"Length of training: {len(training_set)}")
+    print(f"Length of testing: {len(testing_set)}\n")
+
+    training_set = training_set.to_numpy()
+    testing_set = testing_set.to_numpy()
+
+    labels = []
+    for insta_count in range(len(training_set)):
+        labels.append(training_set[insta_count][0])
+    uniqueLabels = np.unique(labels)
+    print(uniqueLabels)
+
+    getEntropy(training_set, uniqueLabels)
+
 
 except IndexError as e:
     print(f"Error. Message below:\n{e}\nPlease try again.")
